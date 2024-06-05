ca2713890ad76607ba43009251270b441494733d
jalalswati <jalalswati00@gmail.com>
2024-06-04 21:22:31 -0700

    Update Actor.cpp

--- IceMan/Actor.cpp
+++ IceMan/Actor.cpp
@@ -4,76 +4,155 @@
 
 bool Agent::annoy(unsigned int amount)
 {
-	if (m_hitPoints <= amount)
-	{
-		m_hitPoints = 0;
-		setDead();
-		return true;
-	}
-	else
-	{
-		m_hitPoints -= amount;
-		return false;
-	}
-}
-
-//ICEMAN__________________________________________________________
-// Actor.cpp
-
-void Iceman::move()
-{
-    if (!isAlive()) return;
-
-    int ch;
-    if (getWorld()->getKey(ch))
+    if (m_hitPoints <= amount)
     {
-        switch (ch)
-        {
-            case KEY_PRESS_ESCAPE:
-                setDead();
-                break;
-            case KEY_PRESS_DOWN:
-                if (getDirection() == down)
-                {
-                    if (moveToIfPossible(getX(), getY() - 1)) // Check and clear ice after moving
-                        getWorld()->clearIce(getX(), getY());
-                }
-                else
-                    setDirection(down);
-                break;
-            case KEY_PRESS_LEFT:
-                if (getDirection() == left)
-                {
-                    if (moveToIfPossible(getX() - 1, getY())) // Check and clear ice after moving
-                        getWorld()->clearIce(getX(), getY());
-                }
-                else
-                    setDirection(left);
-                break;
-            case KEY_PRESS_RIGHT:
-                if (getDirection() == right)
-                {
-                    if (moveToIfPossible(getX() + 1, getY())) // Check and clear ice after moving
-                        getWorld()->clearIce(getX(), getY());
-                }
-                else
-                    setDirection(right);
-                break;
-            case KEY_PRESS_UP:
-                if (getDirection() == up)
-                {
-                    if (moveToIfPossible(getX(), getY() + 1)) // Check and clear ice after moving
-                        getWorld()->clearIce(getX(), getY());
-                }
-                else
-                    setDirection(up);
-                break;
-        }
+        m_hitPoints = 0;
+        setDead();
+        return true;
+    }
+    else
+    {
+        m_hitPoints -= amount;
+        return false;
     }
 }
 
 
+
+//ICEMAN__________________________________________________________
+void Iceman::move()
+{
+    if (!isAlive()) return;
+
+
+    int ch;
+    if (getWorld()->getKey(ch))
+        switch (ch)
+        {
+        case KEY_PRESS_ESCAPE:
+            annoy(100);
+            break;
+
+        case KEY_PRESS_SPACE:
+            if (getWater() > 0) {
+                // more stuff
+                Squirt* s = new Squirt(getWorld(), getX(), getY(), getDirection());
+                getWorld()->addActor(s);
+                getWorld()->playSound(SOUND_PLAYER_SQUIRT);
+                m_water--;
+            }
+            break;
+        case KEY_PRESS_DOWN:
+            if (getDirection() == down)
+                moveToIfPossible(getX(), getY() - 1);
+            else
+                setDirection(down);
+            break;
+        case KEY_PRESS_LEFT:
+            if (getDirection() == left)
+                moveToIfPossible(getX() - 1, getY());
+            else
+                setDirection(left);
+            break;
+        case KEY_PRESS_RIGHT:
+            if (getDirection() == right)
+            {
+                moveToIfPossible(getX() + 1, getY());
+            }
+            else
+                setDirection(right);
+            break;
+        case KEY_PRESS_UP:
+            if (getDirection() == up)
+                moveToIfPossible(getX(), getY() + 1);
+            else
+                setDirection(up);
+            break;
+
+        }
+
+    getWorld()->clearIce(getX(), getY(), getDirection());
+
+}
+
+bool Iceman::annoy(int amount)
+{
+    if (Agent::annoy(amount)) {
+        setDead();
+        getWorld()->playSound(SOUND_PLAYER_GIVE_UP);
+        return true;    // return true if IceMan is completely annoyed
+    }
+    return false;       // still got HP left
+}
+
+
+
 //Boulder-------------------------------------------------------------
+
+//SQUIRT----------------------------------------------------------------
+void Squirt::move()
+{
+    if (!isAlive()) {
+        return;
+    }
+
+    // Check if Squirt annoys any nearby actors
+    if (getWorld()->annoyAllNearbyActors(this, 2, 3)) {
+        setDead();
+        return;
+    }
+
+    // Check if Squirt has traveled its full distance
+    if (m_travelDis == 0) {
+        setDead();
+        return;
+    }
+
+    // Check if Squirt can move one square in the current direction
+    switch (getDirection()) {
+    case up:
+        if (!moveToIfPossible(getX(), getY() + 1)) {
+            setDead();
+            return;
+        }
+        break;
+    case down:
+        if (!moveToIfPossible(getX(), getY() - 1)) {
+            setDead();
+            return;
+        }
+        break;
+    case left:
+        if (!moveToIfPossible(getX() - 1, getY())) {
+            setDead();
+            return;
+        }
+        break;
+    case right:
+        if (!moveToIfPossible(getX() + 1, getY())) {
+            setDead();
+            return;
+        }
+        break;
+    default:
+        break;
+    }
+
+    m_travelDis--;
+
+
+}
+
+void Protestor::move() {
+
+    if (!isAlive()) {
+        return;
+    }
+
+
+    // Implement the movement logic for the Protestor
+}
+
 
 
 // Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
