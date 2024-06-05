1e0c4aa2bf885dd808089278480fab5a0bf1b578
jalalswati <jalalswati00@gmail.com>
2024-06-04 21:23:19 -0700

    Update StudentWorld.h

--- IceMan/StudentWorld.h
+++ IceMan/StudentWorld.h
@@ -14,12 +14,14 @@ class StudentWorld : public GameWorld
 {
 private:
 	Iceman* m_iceman;
-	Ice* iceField[64][64];
+	Ice* m_iceField[64][64];
+	vector<Actor*> m_actors;
+	unsigned int m_barrelsLeft;
+	//vector<Protestor> protestors;
+
 public:
 	StudentWorld(std::string assetDir)
-		: GameWorld(assetDir)
-	{
-	}
+		: GameWorld(assetDir) {}
 
 	virtual int init();
 
@@ -29,6 +31,15 @@ public:
 	{
 	}
 
+	void clearIce(int x, int y, int dir);
+
+	void updateDisplayText();
+
+	unsigned int getBarrelsLeft();
+
+	int annoyAllNearbyActors(Actor* a, int points, int radius);
+
+	void addActor(Actor* a);
 };
 
 #endif // STUDENTWORLD_H_
