af024ce9d29776f0e476491e813ec59b5eb79d2f
jalalswati <jalalswati00@gmail.com>
2024-06-05 17:15:39 -0700

    Update StudentWorld.h

--- IceMan/StudentWorld.h
+++ IceMan/StudentWorld.h
@@ -17,7 +17,7 @@ private:
 	Ice* m_iceField[64][64];
 	vector<Actor*> m_actors;
 	unsigned int m_barrelsLeft;
-	//vector<Protestor> protestors;
+	vector<Protestor*> m_protestors;
 
 public:
 	StudentWorld(std::string assetDir)
@@ -40,6 +40,39 @@ public:
 	int annoyAllNearbyActors(Actor* a, int points, int radius);
 
 	void addActor(Actor* a);
+
+	bool canActorMoveTo(Actor* a, int x, int y) const;
+
+	bool isNearIceman(Actor* a, int radius) const;
+
+	//bool checkRadius(int x, int y, double radius) const;
+
+	bool checkRadius(int x1, int y1, int x2, int y2, double radius) const;
+
+	int intPow(int x) const;
+
+	//bool checkIce(int x, int y, GraphObject::Direction dir);
+
+	bool checkIce(int x, int y);
+
+	bool checkIceBoulder(int x, int y, GraphObject::Direction dir);
+
+	void squirtWater(int x, int y, GraphObject::Direction dir);
+
+	void createBoulders(int numBoulders);
+
+	vector<Protestor*> getProtestors();
+
+	// If the IceMan is within radius of a, return a pointer to the
+// IceMan, otherwise null.
+	//Actor* findNearbyIceMan(Actor* a, int radius) const;
+
+	// If at least one actor that can pick things up is within radius of a,
+	// return a pointer to one of them, otherwise null.
+	//Actor* findNearbyPickerUpper(Actor* a, int radius) const;
+
+
+
 };
 
 #endif // STUDENTWORLD_H_
