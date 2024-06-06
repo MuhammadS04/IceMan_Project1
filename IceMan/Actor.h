d2011d84b71aff2a497b2444e4cba0bdba61bdd2
jalalswati <jalalswati00@gmail.com>
2024-06-06 01:42:53 -0700

    Update Actor.h

--- IceMan/Actor.h
+++ IceMan/Actor.h
@@ -1,278 +1,145 @@
-#ifndef ACTOR_H_
-#define ACTOR_H_
+#ifndef ACTOR_H
+#define ACTOR_H
 
 #include "GraphObject.h"
-using namespace std;
 
 class StudentWorld;
 
-// Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
-
-class Actor : public GraphObject
-{
-private:
-	StudentWorld* world;
-	bool m_isAlive;
+class Actor : public GraphObject {
 public:
-	Actor(StudentWorld* world, int startX, int startY, Direction startDir,
-		bool visible, int imageID, double size, int depth)
-		: GraphObject(imageID, startX, startY, startDir, size, depth), m_isAlive(true), world(world)
-	{
-		setVisible(visible);
-	}
-
-	bool isAlive()
-	{
-		return m_isAlive;
-	}
-
-	virtual void move() = 0;
-
-	StudentWorld* getWorld()
-	{
-		return world;
-	}
-
-	void setDead()
-	{
-		m_isAlive = false;
-		setVisible(false);
-	}
-
-	// Move this actor to x,y if possible, and return true; otherwise,
-	// return false without moving.
-	bool moveToIfPossible(int x, int y)
-	{
-		// Make sure x and y are within the screen
-		if (x < 0 || y < 0 || x >= VIEW_WIDTH - 2 || y >= VIEW_HEIGHT - 3)
-		{
-			return false;
-		}
-
-		////make sure its not blocked
-		//if (getWorld()->isBlocked(x, y))
-		//    return false;
-
-
-		GraphObject::moveTo(x, y);
-		return true;
-
-		//delete this
-		//if (getWorld()->canActorMoveTo(this, x, y))
-		//{
-		//	moveTo(x, y);
-		//	return true;
-		//}
-		//return false;
-	}
-	virtual bool annoy(unsigned int amt)
-	{
-		return true;
-	}
-
-	// Can other actors pass through this actor?
-	virtual bool canActorsPassThroughMe() const
-	{
-		return true;
-	}
-
-	// Can this actor dig through Ice?
-	virtual bool canDigThroughIce() const
-	{
-		return false;
-	}
-
-	// Can this actor pick items up?
-	virtual bool canPickThingsUp() const
-	{
-		return false;
-	}
-
-	// Does this actor hunt the IceMan?
-	virtual bool huntsIceMan() const
-	{
-		return false;
-	}
-
-	// Can this actor need to be picked up to finish the level?
-	virtual bool needsToBePickedUpToFinishLevel() const
-	{
-		return false;
-	}
-};
-
-class Agent : public Actor
-{
-private:
-	unsigned int m_hitPoints;
-public:
-	Agent(StudentWorld* world, int startX, int startY, Direction startDir,
-		int imageID, unsigned int hitPoints)
-		: Actor(world, startX, startY, startDir, true, imageID, 1.0, 0), m_hitPoints(hitPoints)
-	{
-
-	}
-
-	// Pick up a gold nugget.
-	virtual void addGold() = 0;
-
-	// How many hit points does this actor have left?
-	unsigned int getHitPoints() const
-	{
-		return m_hitPoints;
-	}
-
-	virtual bool annoy(unsigned int amount);
-
-
-	virtual bool canPickThingsUp() const
-	{
-		return true;
-	}
-};
-
-class Iceman : public Agent
-{
-private:
-	unsigned int m_gold;
-	unsigned int m_water;
-	unsigned int m_sonar;
-
-public:
-	Iceman(StudentWorld* world, int startX, int startY)
-		:Agent(world, startX, startY, right, IID_PLAYER, 10), m_gold(0), m_sonar(1), m_water(5)
-	{
-		setVisible(true);
-	}
-
-	virtual void move();
-
-	virtual bool annoy(int amount);
-
-	void addGold()
-	{
-		m_gold++;
-	}
-	void addSonar()
-	{
-		m_sonar++;
-	}
-	void addWater(int n)
-	{
-		m_water += n;
-	}
-
-	unsigned int getGold()
-	{
-		return m_gold;
-	}
-
-	unsigned int getSonar() const {
-		return m_sonar;
-	}
-
-	unsigned int getWater() const {
-		return m_water;
-	}
-
-	virtual bool canDigThroughIce()
-	{
-		return true;
-	}
-};
-
-
-
-class Ice : public Actor
-{
-public:
-	Ice(StudentWorld* world, int startX, int startY)
-		: Actor(world, startX, startY, right, true, IID_ICE, .25, 3)
-	{
-		setVisible(true);
-	}
-
-	virtual void move() {};
-};
-
-
-class Boulder : public Actor
-{
-private:
-	int m_state;
-	int m_waitTicks;
-
-	const int STATE_STABLE = 0;
-	const int STATE_WAITING = 1;
-	const int STATE_FALLING = 2;
-
-public:
-	Boulder(StudentWorld* world, int startX, int startY)
-		: Actor(world, startX, startY, down, true, IID_BOULDER, 1.0, 1)
-		, m_state(STATE_STABLE), m_waitTicks(0)
-	{
-		setVisible(true);
-	}
-
-	virtual void move();
-
-	virtual bool canActorsPassThroughMe() const override
-	{
-		return false;
-	}
-
-	bool isIceBelow();
-
-	bool canFallTo(int x, int y) const;
-};
-
-class Squirt : public Actor
-{
-private:
-	unsigned int m_travelDis;
-public:
-	Squirt(StudentWorld* world, int startX, int startY, Direction startDir)
-		: Actor(world, startX, startY, startDir, true, IID_WATER_SPURT, 1.0, 1)
-		, m_travelDis(4)
-	{
-		setVisible(true);
-	}
-
-	virtual void move();
-
-};
- 
-
-class Protestor : public Agent
-{
-public:
-	Protestor(StudentWorld* world, int startX, int startY)
-		: Agent(world, startX, startY, left, IID_PROTESTER, 5) // Assuming a Protestor starts with 5 hit points
-	{
-		// Initialize the stepArray to -1, indicating unvisited cells
-		for (int i = 0; i < VIEW_WIDTH; ++i)
-			for (int j = 0; j < VIEW_HEIGHT; ++j)
-				stepArray[i][j] = -1;
-	}
-
-	virtual void move() override;
-
-	void moveToIfPossible(int x, int y);
-
-	void findShortestPath(const int startX, const int startY, const int finalX, const int finalY);
-
-	virtual void addGold() override {
-		// Implementation for adding gold
-	}
-
-	GraphObject::Direction getDirectionToIceman();
+    Actor(int imageID, int startX, int startY, Direction dir, float size, unsigned int depth, StudentWorld* world);
+    virtual ~Actor();
+    virtual void doSomething() = 0;
+    bool isAlive() const;
+    void setDead();
+    StudentWorld* getWorld() const;
 
 private:
-	int stepArray[VIEW_WIDTH][VIEW_HEIGHT]; // Array to keep track of steps
-	bool didFindPath = false;
-	int step = 0;
-	int m_ticksSinceLastMove;
+    bool m_alive;
+    StudentWorld* m_world;
 };
 
+class Ice : public Actor {
+public:
+    Ice(int startX, int startY, StudentWorld* world);
+    virtual ~Ice();
+    virtual void doSomething();
+};
 
-#endif // ACTOR_H_
+class Iceman : public Actor {
+public:
+    Iceman(StudentWorld* world);
+    virtual ~Iceman();
+    virtual void doSomething();
+
+private:
+    int m_hitPoints;
+    int m_water;
+    int m_gold;
+    int m_sonar;
+};
+
+class Boulder : public Actor {
+public:
+    Boulder(int startX, int startY, StudentWorld* world);
+    virtual ~Boulder();
+    virtual void doSomething();
+
+private:
+    enum State { STABLE, WAITING, FALLING } m_state;
+    int m_waitingTicks;
+};
+
+class Squirt : public Actor {
+public:
+    Squirt(int startX, int startY, Direction dir, StudentWorld* world);
+    virtual ~Squirt();
+    virtual void doSomething();
+
+private:
+    int m_distanceTraveled;
+};
+
+class GoldNugget : public Actor {
+public:
+    GoldNugget(int startX, int startY, bool temporary, StudentWorld* world);
+    virtual ~GoldNugget();
+    virtual void doSomething();
+
+private:
+    bool m_temporary;
+    int m_ticksLeft;
+};
+
+class Protester : public Actor {
+public:
+    Protester(int imageID, int startX, int startY, StudentWorld* world, int hitPoints);
+    virtual ~Protester();
+    virtual void doSomething() = 0;
+    void getAnnoyed(int points);
+    void moveToNextPosition();
+    void setRestingTicks(int ticks);
+    bool isResting() const;
+    void shoutIfCloseToIceman();
+    bool canMoveInDirection(Direction dir) const;
+    void pickNewDirection();
+    void moveToExit();
+
+protected:
+    int m_hitPoints;
+    int m_restingTicks;
+    int m_shoutCooldown;
+    int m_numSquaresToMoveInCurrentDirection;
+    bool m_leaveOilField;
+};
+
+class RegularProtester : public Protester {
+public:
+    RegularProtester(int startX, int startY, StudentWorld* world);
+    virtual ~RegularProtester();
+    virtual void doSomething();
+};
+
+class HardcoreProtester : public Protester {
+public:
+    HardcoreProtester(int startX, int startY, StudentWorld* world);
+    virtual ~HardcoreProtester();
+    virtual void doSomething();
+};
+
+class SonarKit : public Actor {
+public:
+    SonarKit(int startX, int startY, StudentWorld* world);
+    virtual ~SonarKit();
+    virtual void doSomething();
+};
+
+class WaterPool : public Actor {
+public:
+    WaterPool(int startX, int startY, StudentWorld* world);
+    virtual ~WaterPool();
+    virtual void doSomething();
+
+private:
+    int m_ticksLeft;
+};
+
+//==================================CHANGED===================================================
+
+class Barrel : public Actor {
+public:
+    Barrel(int startX, int startY, StudentWorld* world);
+    virtual ~Barrel();
+    virtual void doSomething() override;
+
+private:
+    // Add any necessary private members
+};
+
+//============================================================================================
+
+
+#endif // ACTOR_H
+
+
+
